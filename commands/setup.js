6e9728eb9bb60cb51951cfd8d2b9795ea1aee2c8
ataege <aeasilturk@gmail.com>
2023-02-12 00:49:35 +0300

    feat: add prefix and improvements for setup

--- commands/setup.js
+++ commands/setup.js
@@ -12,49 +12,68 @@ module.exports = {
 	start: async (client, message, args) => {
 		const { guild } = message;
 
-		if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) return;
+		if (
+			!message.member.permissions.has(PermissionsBitField.Flags.Administrator)
+		)
+			return;
 
-		const channel = await guild.channels.create({
-			name: 'adres-bildir',
-			type: 0,
-			permissionOverwrites: [
-				{ id: guild.roles.everyone, deny: [PermissionsBitField.Flags.SendMessages], allow: [PermissionsBitField.Flags.ViewChannel] },
-			],
-		});
+		let channel = guild.channels.cache.find((c) => c.name === 'adres-bildir')
+		
+		if (!channel) {
+			channel = await guild.channels.create({
+				name: 'adres-bildir',
+				type: 0,
+				permissionOverwrites: [
+					{
+						id: guild.roles.everyone,
+						deny: [PermissionsBitField.Flags.SendMessages],
+						allow: [PermissionsBitField.Flags.ViewChannel],
+					},
+				],
+			});
 
-		const logChannel = guild.channels.create({
-			name: 'adres-bildir-log',
-			type: 0,
-			permissionOverwrites: [
-				{
-					id: guild.roles.everyone,
-					deny: [PermissionsBitField.Flags.ViewChannel],
-				},
-			],
-		});
+			const row = new ActionRowBuilder().addComponents(
+				new ButtonBuilder()
+					.setCustomId('adres-paylas')
+					.setLabel('Adres Paylaş')
+					.setStyle(ButtonStyle.Primary),
+				new ButtonBuilder()
+					.setCustomId('nasil-paylasirim')
+					.setLabel('Nasıl Paylaşırım?')
+					.setStyle(ButtonStyle.Success)
+			);
+			const menu = new EmbedBuilder()
+				.setColor(0x0099ff)
+				.setTitle('Adres Paylaşım')
+				.setURL('https://discord.gg/itdepremyardim')
+				.setAuthor({
+					name: 'Adres Paylaş',
+					iconURL: 'https://i.imgur.com/AfFp7pu.png',
+					url: 'https://discord.gg/itdepremyardim',
+				})
+				.setDescription('Adres Paylaşmak için Butona tıkla');
 
-		const row = new ActionRowBuilder().addComponents(
-			new ButtonBuilder()
-				.setCustomId('adres-paylas')
-				.setLabel('Adres Paylaş')
-				.setStyle(ButtonStyle.Primary),
-			new ButtonBuilder()
-				.setCustomId('nasil-paylasirim')
-				.setLabel('Nasıl Paylaşırım?')
-				.setStyle(ButtonStyle.Success)
-		);
-		const menu = new EmbedBuilder()
-			.setColor(0x0099ff)
-			.setTitle('Adres Paylaşım')
-			.setURL('https://discord.gg/itdepremyardim')
-			.setAuthor({
-				name: 'Adres Paylaş',
-				iconURL: 'https://i.imgur.com/AfFp7pu.png',
-				url: 'https://discord.gg/itdepremyardim',
-			})
-			.setDescription('Adres Paylaşmak için Butona tıkla');
+			channel.send({ embeds: [menu], components: [row] });
+		}
 
-		channel.send({ embeds: [menu], components: [row] });
+		message.channel.send(`${channel} adlı kanal bulundu, oluşturulmayacak.`);
+
+		let logChannel = guild.channels.cache.find((c) => c.name === 'adres-bildir-log');
+
+		if (!logChannel) {
+			await guild.channels.create({
+				name: 'adres-bildir-log',
+				type: 0,
+				permissionOverwrites: [
+					{
+						id: guild.roles.everyone,
+						deny: [PermissionsBitField.Flags.ViewChannel],
+					},
+				],
+			});
+		}
+		
+		message.channel.send(`${logChannel} adlı kanal bulundu, oluşturulmayacak.`);
 	},
 
 	name: 'setup',
--- events/message.js
+++ events/message.js
@@ -1,14 +1,14 @@
 const { EmbedBuilder } = require("discord.js");
 
 module.exports = async(message) => {
-
+      const prefix = '!d'
       let client = message.client;
 
-        if (message.author.bot) return;
+        if (message.author.bot || !message.content.startsWith(prefix)) return;
          
         if (!message.guild) return message.reply("Komutlarım sadece sunucularda kullanılabilir.")
       
-        const args = message.content.slice(2).trim().split(/ +/g); 
+        const args = message.content.slice(prefix.length).trim().split(/ +/g); 
         const cmd = args.shift().toLowerCase(); 
       
         if (cmd.length === 0) return; 
