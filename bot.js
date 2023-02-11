d25bc65d6cf24f711083703c77e4cc56d0fd00bd
Ertan Taner <62712555+Ertan222@users.noreply.github.com>
2023-02-11 20:36:49 +0300

    feat: add connection to afetharita backend service (#13)

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 require("dotenv").config();
 const express = require('express');
 const app = express();
@@ -24,27 +25,27 @@ client.on(`interactionCreate`, (interaction) => {
 
   if (interaction.customId == "nasil-paylasirim") {
     const menu = new EmbedBuilder()
-      .setColor(0x0099FF)
-      .setDescription(`
-      BURAYA ÖNEMLİ BİR MESAJ GELECEK
-      `)
+    .setColor(0x0099FF)
+    .setDescription(`
+BURAYA ÖNEMLİ BİR MESAJ GELECEK
+`)
     interaction.reply({ embeds: [menu] , ephemeral: true })
   }
 
   if (interaction.customId == "adres-paylas") {
-    
+
     const modal = new ModalBuilder()
-      .setCustomId('adres-paylasim-modal')
-      .setTitle('Deprem Adres Paylaş');
+    .setCustomId('adres-paylasim-modal')
+    .setTitle('Deprem Adres Paylaş');
 
     const adresbilgi = new TextInputBuilder()
-			.setCustomId('deprem-adresi')
-			.setLabel('Adres Bilgisi')
-			.setPlaceholder('06420 Çankaya/Ankara')
-			.setStyle(TextInputStyle.Paragraph)
-			.setMinLength(15)
-			.setMaxLength(400)
-			.setRequired(true);
+    .setCustomId('deprem-adresi')
+    .setLabel('Adres Bilgisi')
+    .setPlaceholder('06420 Çankaya/Ankara')
+    .setStyle(TextInputStyle.Paragraph)
+    .setMinLength(15)
+    .setMaxLength(400)
+    .setRequired(true);
 
     const modal_1 = new ActionRowBuilder().addComponents(adresbilgi);
 
@@ -67,9 +68,6 @@ client.on(Events.InteractionCreate, interaction => {
       return;
     }
 
-    // TODO: APİYE BİLGİLERİ BURADAN GÖNDERECEĞİZ
-
-
     if(timeouts.find(element => element[0] == interaction.user.id) && !timeouts.find(element => element[1].timeoutEnded)) {
       return interaction.reply({ content: 'Geçiçi olarak engellendiniz', ephemeral: true })
     }
@@ -79,11 +77,11 @@ client.on(Events.InteractionCreate, interaction => {
       try {
         timeouts = timeouts.filter(element => element[0] != interaction.user.id)
       }catch {
-        
+
       }
 
     }
-    
+
     const row = new ActionRowBuilder()
     .addComponents(
       new ButtonBuilder()
@@ -92,27 +90,27 @@ client.on(Events.InteractionCreate, interaction => {
         .setStyle(ButtonStyle.Success),
     )
 
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId('paylasimci-banla-'+interaction.user.id)
-          .setLabel('Paylaşımcıyı Banla')
-          .setStyle(ButtonStyle.Danger),
-      );
+    .addComponents(
+      new ButtonBuilder()
+        .setCustomId('paylasimci-banla-'+interaction.user.id)
+        .setLabel('Paylaşımcıyı Banla')
+        .setStyle(ButtonStyle.Danger),
+    );
 
     const menu = new EmbedBuilder()
-      .setColor(0x0099FF)
-      .setAuthor({ name: 'Adres Paylaş Yönet', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.gg/itdepremyardim' })
-      .setDescription(`
-      
-      Adres; 
-      **${adres ? adres : "Değer Boş"}**
+    .setColor(0x0099FF)
+    .setAuthor({ name: 'Adres Paylaş Yönet', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.gg/itdepremyardim' })
+    .setDescription(`
 
-      Paylaşan: <@${interaction.user.id}>
+Adres; 
+**${adres ? adres : "Değer Boş"}**
 
-      Paylaşımın yapıldığı sunucu adı: **${interaction.guild.name}**
+Paylaşan: <@${interaction.user.id}>
 
-      `)
-      
+Paylaşımın yapıldığı sunucu adı: **${interaction.guild.name}**
+
+`)
+
     let user_id = interaction.user.id //getting user id that filled form
     const guild_id = interaction.guildId; // guild id
     const server = client.guilds.cache.get(guild_id); //getting guild
@@ -120,8 +118,48 @@ client.on(Events.InteractionCreate, interaction => {
     client.channels.cache.get(server_channel.id).send({ embeds: [menu], components: [row] }); // sending message to server_channel
     //ban and timeout buttons interactions
     
+    const response = { 
+        'feeds': [
+          {
+            "raw_text": adres,
+            "channel": "Discord",
+            "extra-parameters": {
+      user_id: interaction.user.id,
+      channel_id: interaction.channelId,
+      username: interaction.user.username,
+      discord_message_id: interaction.message.id,
+      guild_id: interaction.guildId,
+      guild_name: interaction.message.guild.name,
+     message_created_at: interaction.message.createdAt,
+    },
+          "epoch": Date.now()
+            }      
+        ]
+    }
+
+    const Url = `${process.env.BACKEND_GO_API_URL}/events`;        
+    const options = {
+      method: 'POST',
+      headers: { 
+        'Content-Type': 'application/json',
+        "X-Api-Key": process.env.BACKEND_GO_API_KEY
+      },
+      data: JSON.stringify(response),
+      url: Url,
+    };
+
+    try {
+
+      axios(options).then(async function (response) {
+        console.log(1);
+      });
+
+    } catch (err) {
+      console.log(err);
+    }
+
     const filter = i => i.customId.startsWith(`paylasimci-engelle`) || i.customId.startsWith(`paylasimci-banla`) 
-  
+
     const collector = server_channel.createMessageComponentCollector({ filter, time: 864000000 });
 
     collector.once('collect', async i => {
@@ -139,28 +177,28 @@ client.on(Events.InteractionCreate, interaction => {
           Timeout.timeoutEnded = true;
         }, 30*1000);
         timeouts.push([user_id,Timeout]) 
-        
+
       }
       if ((i.customId.startsWith('paylasimci-banla-'))) {
         let member = await server_channel.guild.members.fetch(user_id);
         member.ban({ reason: 'Banned' });
         await i.reply(`User with id ${user_id} has been banned.`)
-        
+
       }
 
 
     })
-    
 
   }
 });
-
 // HEALTHCHECK
 client.login(process.env.TOKEN);
 
 app.use("/healthcheck", require('express-healthcheck')({
-    healthy: () => {
+  healthy: () => {
     return { status: "healthy" }
   }
 }));
-app.listen(80);
+
+
+app.listen(3000);
